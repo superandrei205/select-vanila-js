aa5b1cc50807360916e8bbf64293b86c499491b4
aremizov <aremizov@aori.ru>
2020-08-05 00:52:38 +0400

    initial state

---
+++ index.js
@@ -0,0 +1,4 @@
+import {Select} from './select/select';
+import './select/styles';
+
+const select = new Select()
---
+++ select/select.js
@@ -0,0 +1,13 @@
+export class Select {
+    constructor(selector, options){
+        this.$el = document.querySelector(selector)
+    }
+
+    open(){
+        this.$el.classList.add('open')
+    }
+
+    close(){
+        this.$el.classList.remove('open')
+    }
+}
